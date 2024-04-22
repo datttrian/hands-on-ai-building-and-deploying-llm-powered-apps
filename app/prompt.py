9cc1731f00cb11c9ff3e7138d79c6b7945a32bf1
Dat Tran <datttrian@gmail.com>
2024-04-22 05:42:02 +0000

    Challenge: Set up prompting

---
+++ app/prompt.py
@@ -0,0 +1,6 @@
+##############################################################################
+# Exercise 1:
+# Please navigate and find the Langchain prompt templates used for 
+# RetrievalQAWithSources chain and how it is initialized.
+##############################################################################
+from langchain.prompts import PromptTemplate
